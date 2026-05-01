7f384f9ffa708593efb05f5ef4e98da3ea896c10
ianpatricck <ianpatrick499@gmail.com>
2026-05-01 20:30:15 -0300

    refactor: removed pgsql database

--- phinx.php
+++ phinx.php
@@ -1,34 +1,27 @@
 <?php
 
-(\Dotenv\Dotenv::createImmutable('./'))->safeLoad();
+// \Dotenv\Dotenv::createImmutable("./")->safeLoad();
 
 return [
-    'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/phinx/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/phinx/seeds'
+    "paths" => [
+        "migrations" => "%%PHINX_CONFIG_DIR%%/phinx/migrations",
+        "seeds" => "%%PHINX_CONFIG_DIR%%/phinx/seeds",
     ],
-    'environments' => [
-        'default_migration_table' => 'phinxlog',
-        'default_environment' => $_ENV['APP_ENVIRONMENT'],
-        'production' => [
-            'adapter' => $_ENV['DB_ADAPTER'],
-            'host' => $_ENV['DB_HOST'],
-            'port' => $_ENV['DB_PORT'],
-            'name' => $_ENV['DB_NAME'],
-            'user' => $_ENV['DB_USER'],
-            'pass' => $_ENV['DB_PASSWORD'],
-            'charset' => 'utf8',
+    "environments" => [
+        "default_migration_table" => "phinxlog",
+        "default_environment" => "production",
+        "production" => [
+            "adapter" => "sqlite",
+            "name" => "database",
+            "suffix" => ".db",
+            "charset" => "utf8",
         ],
-        'development' => [
-            'adapter' => 'sqlite',
-            'name' => 'development',
-            'suffix' => '.db',
-            'charset' => 'utf8',
-        ],
-        'testing' => [
-            'adapter' => 'sqlite',
-            'memory' => true
-        ]
+        // "development" => [
+        //     "adapter" => "sqlite",
+        //     "name" => "development",
+        //     "suffix" => ".db",
+        //     "charset" => "utf8",
+        // ],
     ],
-    'version_order' => 'creation'
+    "version_order" => "creation",
 ];
