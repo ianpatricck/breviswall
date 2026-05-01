113f9a04a4dc83ac0150cadda5c2415337b7e889
ianpatricck <ianpatrick499@gmail.com>
2026-05-01 12:26:11 -0300

    feat: created a new app entrypoint

---
+++ public/api.php
@@ -0,0 +1,78 @@
+<?php
+
+// |============================================|
+// | Rotas da API da aplicação                  |
+// |============================================|
+
+use App\Controllers\BoardController;
+use App\Controllers\CardController;
+use App\Controllers\CommentController;
+use App\Controllers\TaskController;
+use App\Controllers\UserController;
+use App\Middlewares\UserAuthorizedMiddleware;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+function runApi($app, $appContainer)
+{
+    /*
+     * Grupo de rotas da API.
+     */
+    $app->group("/api", function ($api) use ($appContainer) {
+        // Controllers
+        $userController = $appContainer->get(UserController::class);
+        $boardController = $appContainer->get(BoardController::class);
+        $cardController = $appContainer->get(CardController::class);
+        $taskController = $appContainer->get(TaskController::class);
+        $commentController = $appContainer->get(CommentController::class);
+
+        /*
+         * Middleware para verificar a autenticidade do usuário.
+         */
+        $userAuthorizedMiddleware = fn(
+            Request $request,
+            RequestHandler $handler,
+        ) => UserAuthorizedMiddleware::handle($request, $handler);
+
+        // Users
+        $api->post("/users/create", [$userController, "create"]);
+        $api->post("/users/login", [$userController, "login"]);
+        $api->get("/users/{by}", [$userController, "findOne"])->add($userAuthorizedMiddleware);
+        $api->patch("/users/email/{by}", [$userController, "updateEmail"])->add($userAuthorizedMiddleware);
+        $api->patch("/users/name/{by}", [$userController, "updateName"])->add($userAuthorizedMiddleware);
+        $api->patch("/users/username/{by}", [$userController, "updateUsername"])->add($userAuthorizedMiddleware);
+        $api->patch("/users/description/{by}", [$userController, "updateBio"])->add($userAuthorizedMiddleware);
+        $api->patch("/users/password/{by}", [$userController, "updatePassword"])->add($userAuthorizedMiddleware);
+        $api->delete("/users/{by}", [$userController, "delete"])->add($userAuthorizedMiddleware);
+
+        // Boards
+        $api->get("/boards", [$boardController, "findMany"])->add($userAuthorizedMiddleware);
+        $api->post("/boards/create", [$boardController, "create"])->add($userAuthorizedMiddleware);
+        $api->put("/boards/{id}", [$boardController, "update"])->add($userAuthorizedMiddleware);
+        $api->delete("/boards/{id}", [$boardController, "delete"])->add($userAuthorizedMiddleware);
+
+        // Cards
+        $api->get("/cards", [$cardController, "findMany"])->add($userAuthorizedMiddleware);
+        $api->get("/cards/{id}", [$cardController, "findOne"])->add($userAuthorizedMiddleware);
+        $api->post("/cards/create", [$cardController, "create"])->add($userAuthorizedMiddleware);
+        $api->put("/cards/{id}", [$cardController, "update"])->add($userAuthorizedMiddleware);
+        $api->delete("/cards/{id}", [$cardController, "delete"])->add($userAuthorizedMiddleware);
+
+        // Tasks
+        $api->get("/tasks/{id}", [$taskController, "findOne"])->add($userAuthorizedMiddleware);
+        $api->get("/tasks", [$taskController, "findMany"])->add($userAuthorizedMiddleware);
+        $api->post("/tasks/create", [$taskController, "create"])->add($userAuthorizedMiddleware);
+        $api->put("/tasks/{id}", [$taskController, "update"])->add($userAuthorizedMiddleware);
+        $api->delete("/tasks/{id}", [$taskController, "delete"])->add($userAuthorizedMiddleware);
+
+        // Comments
+        $api->get("/comments/{id}", [$commentController, "findOne"])->add($userAuthorizedMiddleware);
+        $api->get("/comments", [$commentController, "findMany"])->add($userAuthorizedMiddleware);
+        $api->post("/comments/create", [$commentController, "create"])->add($userAuthorizedMiddleware);
+        $api->patch("/comments/{id}", [$commentController, "update"])->add($userAuthorizedMiddleware);
+        $api->delete("/comments/{id}", [$commentController, "delete"])->add($userAuthorizedMiddleware);
+    });
+
+    /* Iniciando o servidor */
+    $app->run();
+}
---
+++ public/index.php
@@ -0,0 +1,51 @@
+<?php
+
+// |============================================|
+// | Entrypoint da aplicação                    |
+// |============================================|
+
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . "/../src/Utils/isAuthorizedUser.php";
+
+$app = AppFactory::create();
+
+$app->addBodyParsingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+/*
+ * Container que injeta as dependências dos controladores.
+ */
+$appContainerBuilder = new ContainerBuilder();
+$appContainerBuilder->addDefinitions(__DIR__ . "/di-config.php");
+$appContainer = $appContainerBuilder->build();
+
+/*
+ * Configurando o Twig template engine.
+ */
+$twig = Twig::create(__DIR__ . "/", ["cache" => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
+/*
+ * Rota que define o Swagger para documentar a API
+ */
+$app->get("/", function ($request, $response) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, "swagger.html.twig");
+});
+
+/*
+ * Rota para enxergar o arquivo gerado do Swagger com api:generate
+ */
+$app->get("/swagger-json", function ($request, $response) {
+    $swaggerJson = file_get_contents(__DIR__ . "/swagger.json");
+    $response->getBody()->write($swaggerJson);
+    return $response->withStatus(200);
+});
+
+require_once __DIR__ . "/api.php";
+runApi($app, $appContainer);
