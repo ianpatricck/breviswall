c9c0698430f656388cad2b0d3893208f1b124d3c
ianpatricck <ianpatrick499@gmail.com>
2026-07-29 10:53:41 -0300

    chore: removed index.html

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ $appContainer = $appContainerBuilder->build();
 /*
  * Rota que define o Swagger para documentar a API
  */
-$app->get("/api", function ($request, $response) {
+$app->get("/api", function ($_request, $response) {
     $html = file_get_contents(__DIR__ . "/swagger.html");
     $response->getBody()->write($html);
     return $response->withHeader("Content-Type", "text/html");
@@ -38,15 +38,6 @@ $app->get("/swagger-json", function ($_request, $response) {
     $swaggerJson = file_get_contents(__DIR__ . "/swagger.json");
     $response->getBody()->write($swaggerJson);
     return $response->withStatus(200);
-});
-
-/*
- * Rota que exibe o front-end do projeto
- */
-$app->get("/", function ($_request, $response) {
-    $html = file_get_contents(__DIR__ . "/index.html");
-    $response->getBody()->write($html);
-    return $response->withHeader("Content-Type", "text/html");
 });
 
 require_once __DIR__ . "/api.php";
