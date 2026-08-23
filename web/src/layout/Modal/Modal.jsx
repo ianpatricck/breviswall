732e0fc83efa20845f3ec6503c3f031ef003bb2e
ianpatricck <ianpatrick499@gmail.com>
2026-08-22 22:44:41 -0300

    created show/hide Modal logic

--- web/src/layout/Modal/Modal.jsx
+++ web/src/layout/Modal/Modal.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useRef } from "preact/hooks";
 import "./Modal.css";
 
-export function Modal({ children }) {
+export function Modal({ setOpenModal, children }) {
+  const childRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideModalContent = (event) => {
+      if (childRef.current && !childRef.current.contains(event.target)) {
+        setOpenModal(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideModalContent);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideModalContent);
+    };
+  }, []);
+
   return (
     <div className="modal">
-      <div className="modal__content">
+      <div className="modal__content" ref={childRef}>
         {children}
       </div>
     </div>
