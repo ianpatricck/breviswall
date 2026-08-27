c08a86f6b388a08d7dab07a190f5cb3a5c2c719a
ianpatricck <ianpatrick499@gmail.com>
2026-08-27 09:43:59 -0300

    refactor: removed fake data from Canva

--- web/src/layout/Canva/Canva.jsx
+++ web/src/layout/Canva/Canva.jsx
@@ -1,70 +1,18 @@
+import "./Canva.css";
 import { move } from "@dnd-kit/helpers";
 import { DragDropProvider } from "@dnd-kit/react";
-import { useContext, useState } from "preact/hooks";
+import { useContext, useEffect, useState } from "preact/hooks";
 import Card from "@/components/Card/Card";
 import Task from "@/components/Task/Task";
-import "./Canva.css";
 import { ModalContext } from "@/context/ModalContext";
 
-export default function Canva() {
+export default function Canva({ cards, cardsIndex }) {
   const setModal = useContext(ModalContext);
+  const [cardItems, setCardItems] = useState(cards);
 
-  const [cardItems, setCardItems] = useState({
-    backlog: [
-      {
-        id: 1,
-        name: "Fazer bolo de chocolate",
-      },
-      {
-        id: 2,
-        name: "Comprar carne",
-      },
-      {
-        id: 3,
-        name: "Criar agendador de tarefas",
-      },
-
-      {
-        id: 4,
-        name: "Desenvolver Front-End",
-      },
-    ],
-    in_progress: [
-      {
-        id: 5,
-        name: "Organizar pratos",
-      },
-      {
-        id: 6,
-        name: "Preparar o ambiente de homologação",
-      },
-    ],
-    done: [
-      {
-        id: 7,
-        name: "Criar Design da plataforma",
-      },
-      {
-        id: 8,
-        name: "Preparar o ambiente de homologação",
-      },
-    ],
-    review: [],
-    refactor: [
-      {
-        id: 9,
-        name: "Preparar o ambiente de homologação",
-      },
-    ],
-  });
-
-  const cardNames = {
-    backlog: "Backlog",
-    in_progress: "In Progress",
-    done: "Done",
-    refactor: "Refactor",
-    review: "Review",
-  };
+  useEffect(() => {
+    setCardItems(cards);
+  }, [cards]);
 
   return (
     <DragDropProvider
@@ -75,7 +23,7 @@ export default function Canva() {
       }}
     >
       <main className="canva">
-        {Object.entries(cardNames).map(([key, value]) => (
+        {Object.entries(cardsIndex).map(([key, value]) => (
           <Card key={key} id={key} name={value}>
             {cardItems[key].map((item) => (
               <Task
